4e9603f150e3c398c071b51e2de88dfd0e49e92c
MADAI003 <istvan.madarasz@disney.com>
2016-04-20 14:11:14 +0200

    almost paging

--- ingatlan/items.py
+++ ingatlan/items.py
@@ -9,24 +9,28 @@ import scrapy
 
 
 class IngatlanItem(scrapy.Item):
+    id = scrapy.Field()
     cim = scrapy.Field()
-    terulet = scrapy.Field()
-    szobak = scrapy.Field()
-    ar = scrapy.Field()
-    tipus = scrapy.Field()
+    terulet = scrapy.Field()    # int
+    szobak = scrapy.Field()     # [int (egesz szoba), int (felszoba)]
+    ar = scrapy.Field()         # int
+    tipus = scrapy.Field()      # tegla, panel, csusztatott zsalus
 
-    allapot = scrapy.Field()
-    komfort = scrapy.Field()
-    emelet = scrapy.Field()
+    allapot = scrapy.Field()    # jo allapotu, felujitott, ujszeru, jo allapotu
+    komfort = scrapy.Field()    # osszkomfortos, duplakomfortos
+    emelet = scrapy.Field()     # foldszint, felemelet, int
     szintek = scrapy.Field()
     lift = scrapy.Field()
     belmagassag = scrapy.Field()
-    futes = scrapy.Field()
+    futes = scrapy.Field()  # gaz (konvertor), gaz (cirko), kozponti egyedi mukodessel, kozponti,
     legkondi = scrapy.Field()
     akadaly = scrapy.Field()
     wc = scrapy.Field()
     tajolas = scrapy.Field()
-    kilatas = scrapy.Field()
+    kilatas = scrapy.Field()    # udvari, kertre nezo, utcai
     kert = scrapy.Field()
-    tetoter = scrapy.Field()
+    tetoter = scrapy.Field()    # nem, nem tetoteri, igen
     parkolas = scrapy.Field()
+    erkely = scrapy.Field()
+
+    mezok = scrapy.Field()
--- ingatlan/spiders/spider.py
+++ ingatlan/spiders/spider.py
@@ -3,13 +3,16 @@ from ingatlan.items import IngatlanItem
 from scrapy.linkextractors import LinkExtractor
 from scrapy.contrib.spiders import CrawlSpider
 from scrapy_splash import SplashRequest
+from scrapy.spiders import Rule
 
 class IngatlanLinkSpider(CrawlSpider):
     name = "Link"
     allowed_domains = ["ingatlan.com"]
     start_urls = [
-        "http://ingatlan.com/lista/elado+lakas+xiii-ker"
+        "http://ingatlan.com/listasz/elado+lakas+tegla-epitesu-lakas+xiii-ker"
     ]
+
+    rules = (Rule(LinkExtractor(restrict_xpaths="//a[@class='button next']"),),)
 
     def parse(self, response):
         le = LinkExtractor(allow="", restrict_xpaths="//a[contains(@class,'rowclick')]", unique=True)
@@ -27,6 +30,7 @@ class IngatlanLinkSpider(CrawlSpider):
     def parse_ingatlan(self, response):
         item = IngatlanItem()
 
+        item['id'] = response.xpath("//b[@class='listing-id']/text()").extract_first()
         item['cim'] = response.xpath('//h1/text()').extract_first()
         item['tipus'] = response.xpath("//div[@class='listing-subtype']/text()").extract_first()
         item['terulet'] = self.process_terulet(
@@ -36,48 +40,37 @@ class IngatlanLinkSpider(CrawlSpider):
         item['ar'] = self.process_ar(
             response.xpath("//div[contains(@class, 'parameter-price')]/span[2]/text()").extract_first())
 
-        item['allapot'] = response.xpath(
-            "//div[@class='paramterers']/table[1]/tbody/tr[1]/td[2]/text()").extract_first()
-        item['komfort'] = response.xpath(
-            "//div[@class='paramterers']/table[1]/tbody/tr[2]/td[2]/text()").extract_first()
-        item['emelet'] = response.xpath(
-            "//div[@class='paramterers']/table[1]/tbody/tr[3]/td[2]/text()").extract_first()
-        item['szintek'] = response.xpath(
-            "//div[@class='paramterers']/table[1]/tbody/tr[4]/td[2]/text()").extract_first()
-        item['lift'] = response.xpath(
-            "//div[@class='paramterers']/table[1]/tbody/tr[5]/td[2]/text()").extract_first()
-        item['belmagassag'] = response.xpath(
-            "//div[@class='paramterers']/table[1]/tbody/tr[6]/td[2]/text()").extract_first()
-        item['futes'] = response.xpath(
-            "//div[@class='paramterers']/table[1]/tbody/tr[7]/td[2]/text()").extract_first()
-        item['legkondi'] = response.xpath(
-            "//div[@class='paramterers']/table[1]/tbody/tr[8]/td[2]/text()").extract_first()
+        item['allapot'] = response.xpath("//td[contains(text(),'llapota')]/../td[2]/text()").extract_first()
+        item['komfort'] = response.xpath("//td[contains(text(),'Komfort')]/../td[2]/text()").extract_first()
+        item['emelet'] = response.xpath("//td[contains(text(),'Emelet')]/../td[2]/text()").extract_first()
+        item['szintek'] = response.xpath("//td[contains(text(),'szintjei')]/../td[2]/text()").extract_first()
+        item['lift'] = response.xpath("//td[contains(text(),'lift')]/../td[2]/text()").extract_first()
+        item['belmagassag'] = response.xpath("//td[contains(text(),'Belmagass')]/../td[2]/text()").extract_first()
+        item['futes'] = response.xpath("//td[contains(text(),'F\u0171t')]/../td[2]/text()").extract_first()
+        item['legkondi'] = response.xpath("//td[contains(text(),'gkondicion')]/../td[2]/text()").extract_first()
 
-        item['akadaly'] = response.xpath(
-            "//div[@class='paramterers']/table[2]/tbody/tr[1]/td[2]/text()").extract_first()
-        item['wc'] = response.xpath(
-            "//div[@class='paramterers']/table[2]/tbody/tr[2]/td[2]/text()").extract_first()
-        item['tajolas'] = response.xpath(
-            "//div[@class='paramterers']/table[2]/tbody/tr[3]/td[2]/text()").extract_first()
-        item['kilatas'] = response.xpath(
-            "//div[@class='paramterers']/table[2]/tbody/tr[4]/td[2]/text()").extract_first()
-        item['kert'] = response.xpath(
-            "//div[@class='paramterers']/table[2]/tbody/tr[5]/td[2]/text()").extract_first()
-        item['tetoter'] = response.xpath(
-            "//div[@class='paramterers']/table[2]/tbody/tr[6]/td[2]/text()").extract_first()
-        item['parkolas'] = response.xpath(
-            "//div[@class='paramterers']/table[2]/tbody/tr[7]/td[2]/text()").extract_first()
+        item['akadaly'] = response.xpath("//td[contains(text(),'lymentes')]/../td[2]/text()").extract_first()
+        item['wc'] = response.xpath("//td[contains(text(),'s WC')]/../td[2]/text()").extract_first()
+        item['tajolas'] = response.xpath("//td[contains(text(),'jol')]/../td[2]/text()").extract_first()
+        item['kilatas'] = response.xpath("//td[contains(text(),'Kil')]/../td[2]/text()").extract_first()
+        item['kert'] = response.xpath("//td[contains(text(),'Kertkapcsolatos')]/../td[2]/text()").extract_first()
+        item['tetoter'] = response.xpath("//td[contains(text(),'Tet\u0151t')]/../td[2]/text()").extract_first()
+        item['parkolas'] = response.xpath("//td[contains(text(),'Parkol')]/../td[2]/text()").extract_first()
+        item['erkely'] = self.process_terulet(
+            response.xpath("//td[contains(text(),'Erk')]/../td[2]/text()").extract_first(default='0'))
+
+        # item['mezok'] = response.xpath("//div[@class='paramterers']/table/tbody/tr/td[1]/text()").extract()
 
         for key, value in item.items():
             if isinstance(value, str) and value.isnumeric():
                 item[key] = int(value)
-            if value == 'nincs megadva':
+            if value == 'nincs megadva' or value == None:
                 item[key] = ''
 
         yield item
 
     def process_terulet(self, text):
-        return int(text.split()[0])
+        return int(text.split()[0].split('.')[0])
 
     def process_ar(self, text):
         return int(float(text.split()[0].replace(',', '.')) * 1000000)
